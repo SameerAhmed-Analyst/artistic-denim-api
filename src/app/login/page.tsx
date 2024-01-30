ca898397e68b1c47f962566f54f973546d62f51c
SameerAhmed-Analyst <154601105+SameerAhmed-Analyst@users.noreply.github.com>
2024-01-30 12:35:17 +0500

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -79,7 +79,7 @@ export default function Page() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-[#1b2d92] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                <a href="/">Sign in</a>
+                <a href="/powerhouse1">Sign in</a>
               </button>
             </div>
           </form>
