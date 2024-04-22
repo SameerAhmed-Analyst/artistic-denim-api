c8ee9ec738824b67d2a7a939528ab94495eaf166
SameerAhmed-Analyst <154601105+SameerAhmed-Analyst@users.noreply.github.com>
2024-04-22 21:57:07 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -256,7 +256,7 @@ export default function Home() {
                       textAlign: "center",
                     }}
                   >
-                    {/* {percentageUsedDataEPH1}% */}
+                    {percentageUsedDataEPH1}%
                   </div>
                   <div
                     style={{
