44cdf11a660bd57e8099f746172406a1faa68d3e
SameerAhmed-Analyst <154601105+SameerAhmed-Analyst@users.noreply.github.com>
2024-04-26 12:07:50 +0500

    feat: machine card

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -7,32 +7,60 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import CustomCard from '@/components/customcard'
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div className="mt-10">
-      <Card className="rounded-md m-2 p-2 bg-black">
-        <CardContent className="flex justify-between pb-0">
-          <p className="text-white">Weaving</p>
-          <p className="text-white">71</p>
-          <p className="text-white">Status</p>
-        </CardContent>
-      </Card>
-      <Card className="rounded-none m-2 p-2">
-        <CardContent className="flex justify-between pb-0">
-          <p>Spinning</p>
-          <p>40</p>
-          <p>Status</p>
-        </CardContent>
-      </Card>
-      <Card className="rounded-none m-2 p-2">
-        <CardContent className="flex justify-between pb-0">
-          <p>Dyeing</p>
-          <p>2</p>
-          <p>Status</p>
-        </CardContent>
-      </Card>
-    </div>
+    <>
+      <div className="mt-10 grid grid-cols-1">
+        <Card className="rounded-md m-2 p-2 bg-black">
+          <CardContent className="flex justify-between pb-0">
+            <p className="text-white">Weaving</p>
+            <p className="text-white">71</p>
+          </CardContent>
+        </Card>
+        <Card className="rounded-none m-2 p-2">
+          <CardContent className="flex justify-evenly pb-0">
+            <div className=" flex items-center space-x-4 p-2">
+              <Image
+                src={"/air-jet-loom-500x500.jpg"}
+                width={100}
+                height={100}
+                alt={""}
+              />
+              <div className="flex-1 space-y-1">
+                <p className="text-sm font-semibold leading-none">Weaving</p>
+                <p className="text-sm text-muted-foreground">
+                  Currently 120 Machines Working.
+                </p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="grid grid-cols-2">
+        <Card className="rounded-none m-2 p-2">
+          <CardContent className="pb-0 space-y-2">
+            <p className="text-base text-center font-semibold leading-none">
+              Weaving
+            </p>
+            <div className="flex justify-center space-x-1">
+              <span className="h-2 w-2 translate-y-1.5 rounded-full bg-green-600" />
+              <p className="text-sm text-muted-foreground">
+                Active 85
+                <span className="text-sm text-muted-foreground">/300</span>
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+        <CustomCard title={"Dyeing"} active={2} total={2} />
+        <CustomCard title={"Rebeamer"} active={24} total={30} />
+        <CustomCard title={"Ball Warping"} active={1} total={1} />
+        <CustomCard title={"Mercerize"} active={1} total={1} />
+        <CustomCard title={"Sizing"} active={5} total={6} />
+      </div>
+    </>
   );
 };
 
