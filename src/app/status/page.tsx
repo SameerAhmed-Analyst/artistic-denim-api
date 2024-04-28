fe8d443389f5689f7b3274c2aa7482488ae66f79
SameerAhmed-Analyst <154601105+SameerAhmed-Analyst@users.noreply.github.com>
2024-04-28 15:58:58 +0500

    Update page.tsx

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -9,6 +9,7 @@ const page = () => {
         <MachineCategoryCard title={"Mercerize"} active={2} total={2} image="mercerize.png" />
         <MachineCategoryCard title={"Sizing"} active={2} total={2} image="sizing.png" />
         <MachineCategoryCard title={"Ball Warping"} active={120} total={340} image="loom.jpg" />
+        <p className="-z-10 absolute top-2/4 left-[15%] text-blue-300 font-bold -rotate-45 text-5xl">Development</p>
       </div>
     </>
   );
