c86490be244bd9a57e1a6ff51c2414836aee60c7
SameerAhm-ed <154601105+SameerAhmed-Analyst@users.noreply.github.com>
2025-02-20 23:14:53 +0500

    Update page.tsx

--- src/app/(electric_consumption)/luwa/page.tsx
+++ src/app/(electric_consumption)/luwa/page.tsx
@@ -10,7 +10,8 @@ export interface LuwaData {
   id: number;
   luwa_1_kW: number;
   luwa_2_kW: number;
-  luwa_3_kW: number;
+  luwa_3A_kW: number;
+  luwa_3B_kW: number;
   luwa_4_kW: number;
   luwa_5_kW: number;
   luwa_6_kW: number;
@@ -104,11 +105,7 @@ const Page = () => {
   useEffect(() => {
     if (data.length > 0) {
       const values = data.map((item) => item.luwa_1_kW); // Updated to LUWA 1 kW
-      const percentageUsed = initializeChart(
-        "luwa1",
-        values,
-        250
-      );
+      const percentageUsed = initializeChart("luwa1", values, 250);
       setPercentageUsedDataL1(percentageUsed);
     }
   }, [data]);
@@ -116,11 +113,23 @@ const Page = () => {
   useEffect(() => {
     if (data.length > 0) {
       const values = data.map((item) => item.luwa_2_kW); // Updated to LUWA 2 kW
-      const percentageUsed = initializeChart(
-        "luwa2",
-        values,
-        250
-      );
+      const percentageUsed = initializeChart("luwa2", values, 250);
+      setPercentageUsedDataL2(percentageUsed);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      const values = data.map((item) => item.luwa_3A_kW); // Updated to LUWA 2 kW
+      const percentageUsed = initializeChart("luwa3A", values, 250);
+      setPercentageUsedDataL2(percentageUsed);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      const values = data.map((item) => item.luwa_3B_kW); // Updated to LUWA 2 kW
+      const percentageUsed = initializeChart("luwa3B", values, 250);
       setPercentageUsedDataL2(percentageUsed);
     }
   }, [data]);
@@ -230,6 +239,116 @@ const Page = () => {
                 return (
                   <div key={item.id} className="pt-3 text-base font-bold">
                     <p>Load {item.luwa_2_kW} kW</p>
+                  </div>
+                );
+              })}
+              <p className="text-xs text-muted-foreground">
+                250 total capacity in KW
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="p-0">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 p-4">
+            <CardTitle className="text-xl font-bold">LUWA 3A</CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+            </svg>
+          </CardHeader>
+          <CardContent className="flex justify-evenly">
+            <div
+              style={{
+                width: "100px",
+                height: "100px",
+                float: "left",
+                position: "relative",
+              }}
+            >
+              <div
+                style={{
+                  width: "100%",
+                  height: "40px",
+                  position: "absolute",
+                  top: "55%",
+                  left: "0",
+                  marginTop: "-20px",
+                  lineHeight: "19px",
+                  textAlign: "center",
+                }}
+              >
+                {data.map((item) => item.luwa_3A_kW.toFixed(0))} kW
+              </div>
+              <canvas id="luwa3A" width="100" height="100" />
+            </div>
+            <div className="">
+              {data.map((item) => {
+                return (
+                  <div key={item.id} className="pt-3 text-base font-bold">
+                    <p>Load {item.luwa_3A_kW} kW</p>
+                  </div>
+                );
+              })}
+              <p className="text-xs text-muted-foreground">
+                250 total capacity in KW
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="p-0">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 p-4">
+            <CardTitle className="text-xl font-bold">LUWA 3B</CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+            </svg>
+          </CardHeader>
+          <CardContent className="flex justify-evenly">
+            <div
+              style={{
+                width: "100px",
+                height: "100px",
+                float: "left",
+                position: "relative",
+              }}
+            >
+              <div
+                style={{
+                  width: "100%",
+                  height: "40px",
+                  position: "absolute",
+                  top: "55%",
+                  left: "0",
+                  marginTop: "-20px",
+                  lineHeight: "19px",
+                  textAlign: "center",
+                }}
+              >
+                {data.map((item) => item.luwa_3B_kW.toFixed(0))} kW
+              </div>
+              <canvas id="luwa3B" width="100" height="100" />
+            </div>
+            <div className="">
+              {data.map((item) => {
+                return (
+                  <div key={item.id} className="pt-3 text-base font-bold">
+                    <p>Load {item.luwa_3B_kW} kW</p>
                   </div>
                 );
               })}
