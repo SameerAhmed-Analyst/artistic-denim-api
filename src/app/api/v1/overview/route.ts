f4acb3a33ea5c98038be10a6af52ca8e25c42138
SameerAhm-ed <154601105+SameerAhmed-Analyst@users.noreply.github.com>
2025-02-27 22:39:08 +0500

    diesel and tubine added in overview

--- src/app/api/v1/overview/route.ts
+++ src/app/api/v1/overview/route.ts
@@ -1,29 +1,38 @@
-import { NextRequest, NextResponse } from "next/server"
-import { config } from "@/db/dbconfig"
-const sql = require('mssql')
+import { NextRequest, NextResponse } from "next/server";
+import { config } from "@/db/dbconfig";
+const sql = require('mssql');
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
 
 export const GET = async () => {
-
-    const pool = await sql.connect(config);
+    const poolPromise = sql.connect(config);
 
     try {
-        const result1 = await pool.request().query('SELECT * FROM OVERVIEW');
-        const result2 = await pool.request().query('SELECT powerhouse1gen, powerhouse2gen, powerhouse3gen, AM17_PH2, totalpowergen FROM dashboard');
-        const result3 = await pool.request().query('SELECT MAN_KW, MAK1_KW, MAK2_KW FROM powerhouse3');
-        
+        const pool = await poolPromise;
+
+        // Run all queries in parallel using Promise.all
+        const [result1, result2, result3, result4, result5] = await Promise.all([
+            pool.request().query('SELECT * FROM OVERVIEW'),
+            pool.request().query('SELECT powerhouse1gen, powerhouse2gen, powerhouse3gen, AM17_PH2, totalpowergen FROM dashboard'),
+            pool.request().query('SELECT MAN_KW, MAK1_KW, MAK2_KW FROM powerhouse3'),
+            pool.request().query('SELECT turbinekw FROM powerhouse2'),
+            pool.request().query('SELECT engine6kw, engine7kw FROM powerhouse1')
+        ]);
+
         // Combine results
         const combinedResults = {
             overview: result1.recordset,
             dashboard: result2.recordset,
-            powerhouse3: result3.recordset
+            powerhouse3: result3.recordset,
+            powerhouse2: result4.recordset,
+            powerhouse1: result5.recordset
         };
-    
+
+        // Return the combined results as JSON
         return NextResponse.json({ data: combinedResults });
+
     } catch (error) {
-        console.log("Error querying data: ", error);
+        console.error("Error querying data: ", error);
         return NextResponse.json({ error: "Error querying data" });
     }
-    
-}
+};
