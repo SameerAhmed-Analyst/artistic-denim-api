84b43676e093c493005ca9d554934f7d65ea7174
SameerAhm-ed <154601105+SameerAhmed-Analyst@users.noreply.github.com>
2025-02-28 14:22:34 +0500

    Update page.tsx

--- src/app/custom_report/page.tsx
+++ src/app/custom_report/page.tsx
@@ -277,7 +277,7 @@ export default function Home() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-xl md:text-2xl font-bold mb-4 text-center">
-        Report Generate
+        Generate Report
       </h1>
 
       {/* Date Pickers (Responsive Layout) */}
