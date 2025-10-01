9a3b96c2d70cfcd6f1d64ae94c6b48df5f647ffd
SameerAhm-ed <154601105+SameerAhmed-Analyst@users.noreply.github.com>
2025-10-01 14:46:21 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,17 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   }
 });
 
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        // source: "/api/proxy/:path*",
+        // destination: "http://ems.am5pearl.com:8000/:path*",
+        source: '/query', // The endpoint in your frontend you want to rewrite
+        destination: 'http://ems.am5pearl.com:5000/query', // The target API URL
+      },
+    ];
+  },
+};
 
 module.exports = withPWA(nextConfig);
